94f7964d3e3fb278322e922e6e4b5292fc5cd7f4
Dmitry Shepelin <shepelin.d@ya.ru>
2020-07-05 19:13:33 +0300

    Another try

--- support/__init__.py
+++ support/__init__.py
@@ -18,7 +18,7 @@ def clear_records(dataset: pd.DataFrame, test_dataset: pd.DataFrame, shops, item
     dataset = dataset.drop('date', axis=1).join(items[['item_category_id']], on='item_id')
 
     for i in ['shop_id', 'item_id', 'date_block_num', 'item_cnt_day', 'item_price']:
-        dataset[i] = dataset[i].astype('int32')
+        dataset[i] = dataset[i].astype('int16')
 
     # Shops fix
     for pair in [(0, 57), (1, 58), (10, 11)]:
@@ -45,11 +45,11 @@ def clear_records(dataset: pd.DataFrame, test_dataset: pd.DataFrame, shops, item
 
 
 def get_prices_means(dataset: pd.DataFrame):
-    item_mean_prices = dataset.groupby('item_id')['item_price'].mean().fillna(0).astype('int32')
+    item_mean_prices = dataset.groupby('item_id')['item_price'].mean().fillna(0).astype('int16')
     item_mean_prices.name = 'item_mean_prices'
-    shop_mean_prices = dataset.groupby('shop_id')['item_price'].mean().fillna(0).astype('int32')
+    shop_mean_prices = dataset.groupby('shop_id')['item_price'].mean().fillna(0).astype('int16')
     shop_mean_prices.name = 'shop_mean_prices'
-    cat_mean_prices = dataset.groupby('item_category_id')['item_price'].mean().fillna(0).astype('int32')
+    cat_mean_prices = dataset.groupby('item_category_id')['item_price'].mean().fillna(0).astype('int16')
     cat_mean_prices.name = 'cat_mean_prices'
 
     print("Mean prices are found...")
@@ -57,7 +57,7 @@ def get_prices_means(dataset: pd.DataFrame):
     return item_mean_prices, shop_mean_prices, cat_mean_prices
 
 
-def generate_zero_sales(dataset, sample_size=1500):
+def generate_zero_sales(dataset, sample_size=800):
     print("Generating zero sales...")
 
     unique_shops = dataset['shop_id'].unique()
@@ -89,7 +89,7 @@ def group_records(dataset):
                                                                                          'item_cnt_day'],
                                                                                 ), ignore_index=True)
 
-    data_grouped['revenue'] = (data_grouped['item_price'] * data_grouped['item_cnt_day']).astype('int32')
+    data_grouped['revenue'] = (data_grouped['item_price'] * data_grouped['item_cnt_day']).astype('int16')
 
     data_grouped.drop('item_price', axis=1, inplace=True)
 
@@ -123,7 +123,7 @@ def lag_features(full, train_mask, story_len, lag_columns, index_columns=['date_
 
     # Cycle through length
     for k in tqdm([1, 2, 4, 6, 12]):
-        gr_copy = (full[full['date_block_num'] < train_mask])[lag_columns + index_columns].copy()
+        gr_copy = (full[full['date_block_num'] < train_mask])[lag_columns + index_columns].astype('int16')
 
         # Merging is done with use of lagging previous records
         gr_copy['date_block_num'] += k
@@ -146,12 +146,28 @@ def set_categories_features(full, train_mask, category_columns):
     print("Calculating categories' features...")
 
     # One Hot
-    full = full.join(pd.get_dummies(full['item_category_id'], prefix='category'))
+    full = full.join(pd.get_dummies(full['shop_id'], prefix='shop'))
+
+    # Count mean values
+    sample = full[full['date_block_num'] < train_mask].groupby(['date_block_num'])[
+        ['target']].mean().astype('float16')
+    sample.rename(columns={'target': 'target_month_mean'}, inplace=True)
+
+    # Mean encoding join
+    full = full.join(sample['target_month_mean'], on=['date_block_num'])
+
+    # Count mean values
+    sample = full[full['date_block_num'] < train_mask].groupby(['date_block_num', 'shop_id', 'item_category_id'])[
+        ['target']].mean().astype('float16')
+    sample.rename(columns={'target': 'shop_cat' + '_target_mean'}, inplace=True)
+
+    # Mean encoding join
+    full = full.join(sample['shop_cat' + '_target_mean'], on=['date_block_num', 'shop_id', 'item_category_id'])
 
     for category_column in category_columns:
         # Count mean values
         sample = full[full['date_block_num'] < train_mask].groupby(['date_block_num', category_column])[
-            ['target']].mean()
+            ['target']].mean().astype('float16')
         sample.rename(columns={'target': category_column + '_target_mean'}, inplace=True)
 
         # Mean encoding join
@@ -159,7 +175,7 @@ def set_categories_features(full, train_mask, category_columns):
 
         # Count mean values
         sample = full[full['date_block_num'] < train_mask].groupby(['date_block_num', category_column])[
-            ['revenue']].mean()
+            ['revenue']].sum().astype('int32')
         sample.rename(columns={'revenue': category_column + '_revenue_mean'}, inplace=True)
 
         # Mean encoding join
@@ -168,7 +184,7 @@ def set_categories_features(full, train_mask, category_columns):
         # Frequency encoding
         sample = full[full['date_block_num'] < train_mask].groupby([
             'date_block_num', category_column]).size() / full[full['date_block_num'] < train_mask].groupby(
-            'date_block_num').size()
+            'date_block_num').size().astype('float16')
         sample.name = category_column + '_freq'
 
         full = full.join(sample, on=['date_block_num', category_column])
@@ -214,7 +230,11 @@ def different_items_sold(full):
 def validation_preparation(full, train_mask, story_len, to_join, join=True):
     category_columns = ['shop_id', 'item_id', 'item_category_id']
 
+    print(full.shape)
+
     full = set_categories_features(full, train_mask, category_columns)
+
+    print(full.shape)
 
     if join:
         for i in to_join:
@@ -222,13 +242,18 @@ def validation_preparation(full, train_mask, story_len, to_join, join=True):
 
     full = different_items_sold(full)
 
+    print(full.shape)
+
     full = lag_features(full, train_mask, story_len,
-                        lag_columns=['target', 'revenue', 'unique_items_sold_by_shop'] + [
+                        lag_columns=['shop_cat_target_mean', 'target_month_mean', 'target', 'revenue', 'unique_items_sold_by_shop'] + [
                             i + '_freq' for i in category_columns] + [i + '_target_mean' for i in category_columns] + [
                             i + '_revenue_mean' for i in category_columns])
 
+    print(full.shape)
+
     n_sales(full, story_len)
 
+    '''
     numerical_columns = ['shop_id_freq_lag_{}'.format(i) for i in [1, 2, 4, 6, 12]] + [
         'item_id_freq_lag_{}'.format(i) for i in [1, 2, 4, 6, 12]] + [
                             'item_category_id_freq_lag_{}'.format(i) for i in [1, 2, 4, 6, 12]] + [
@@ -240,9 +265,10 @@ def validation_preparation(full, train_mask, story_len, to_join, join=True):
     }
     
     numerical_features(full, numerical_columns, functions)
+    '''
 
     # "category_columns" consists of nominal values, so they are mostly useless for any model
-    full.drop(category_columns + ['revenue', 'unique_items_sold_by_shop'] + [i + '_freq' for i in category_columns] + [
+    full.drop(category_columns + ['shop_cat_target_mean', 'revenue', 'target_month_mean', 'unique_items_sold_by_shop'] + [i + '_freq' for i in category_columns] + [
             i + '_target_mean' for i in category_columns] + [i + '_revenue_mean' for i in category_columns],
             axis=1, inplace=True)
 
